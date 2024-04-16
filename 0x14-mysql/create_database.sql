4694c038163af6c8d29411233fbf0ad27746e60d
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-16 16:45:42 -0700

    2. If only you could see what I've seen with your eyes

---
+++ 0x14-mysql/create_database.sql
@@ -0,0 +1,11 @@
+-- Script that create a database and add a table to it
+-- cat create_database.sql | mysql -hlocalhost -uroot -p
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE IF NOT EXISTS nexus6 (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256)
+);
+INSERT INTO nexus6 (name) VALUES ('leon');
+GRANT SELECT ON tyrell_corp.nexus6 TO holberton_user@localhost;
+FLUSH PRIVILEGES;
