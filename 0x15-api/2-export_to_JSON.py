98daeeb8f84867344e280001695dc7c95608ce0f
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-22 15:31:26 -0700

    2. Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -14,12 +14,12 @@ if __name__ == "__main__":
     ids = {"userId": user_id}
     todos = requests.get(url + "todos", params=ids).json()
     file_name = "{}.json".format(user_id)
-    todos_list = []
+    user_dict = {user_id: []}
     for todo in todos:
-        todos_list.append({"task": todo.get(
+        todo_dict = {"task": todo.get(
             "title"), "completed": todo.get(
                 "completed"), "username": user_response.get(
-                    "username")})
-    user_dict = '{{{}: {}}}'.format(user_id, todos_list)
+                    "username")}
+        user_dict[user_id].append(todo_dict)
     with open(file_name, "w") as file:
         json.dump(user_dict, file)
