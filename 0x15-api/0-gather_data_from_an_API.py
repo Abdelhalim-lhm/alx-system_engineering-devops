32d2b7887d7acc2d8a8b2acb3ec75816044d565f
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-22 13:06:14 -0700

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script to get data from a REST API """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(user_id)).json()
+    ids = {"userId": user_id}
+    todos = requests.get(url + "todos", params=ids).json()
+    task_done = []
+    for todo in todos:
+        if todo.get("completed") is True:
+            task_done.append(todo.get("title"))
+    print("Employee {} is done with tasks({}/{})".format(
+        user_response.get("name"), len(task_done), len(todos)))
+    for done in task_done:
+        print("\t {}".format(done))
